61901b8f09e6b9da07f190f718e832a4a9cf7b40
Gabriel <gabriel.wambugu@student.moringaschool.com>
2023-03-15 07:54:27 +0300

    lab done

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,24 @@
 #write your code here
 
-def countdown
+def countdown number
+    response = "HAPPY NEW YEAR!"
+    while number > 0
+        puts "#{number} SECOND(S)!"
+        number = number - 1
+    end
+     return response
 end
+
+
+def countdown_with_sleep number
+    while number > 0
+    
+        puts "#{number} SECOND(S)!"
+        number = number - 1
+        sleep(1)
+    end
+
+end
+
+countdown 12
+countdown_with_sleep 10
